5798bed5f1ce39f8226e6602848811f455f531f8
entriphy <1747088+entriphy@users.noreply.github.com>
2025-03-04 11:14:03 -0800

    Use SnowballStemmer instead of PorterStemmer, update symbols
    SnowballStemmer includes other languages

--- words.py
+++ words.py
@@ -3,7 +3,7 @@ import csv
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
+from nltk.stem import SnowballStemmer
 
 LANGUAGE_MAPPING = {
     "en": "english",
@@ -13,12 +13,19 @@ LANGUAGE_MAPPING = {
     "de": "german"
 }
 
-SYMBOLS = {"|", "-", ",", ".", "+", ":", "?", "!", '"', "'", "(", ")", "[", "]", "...", "$"}
+SYMBOLS = {"|", "-", ",", ".", "+", ":", "?", "!", '"', "'", "(", ")", "[", "]", "...", "$", "‘", "&", "“", "–", "„", "©", "«", "»", "’", "”", "/", "•", "™", "--"}
 
-# Initialize Stemmer
-stemmer = PorterStemmer()
 
-def tokenize_document(filename: str, lang: str) -> list[str]:
+def map_language(lang: str) -> str:
+    if lang in LANGUAGE_MAPPING:
+        language = LANGUAGE_MAPPING[lang]
+    else:
+        print(f"WARNING: No language mapping for {lang}; defaulting to English (en)...")
+        language = "english"
+    return language
+
+
+def tokenize_document(filename: str, language: str) -> list[str]:
     # Read file and parse with BeautifulSoup
     with open(filename, "r", encoding="utf-8") as f:
         contents = f.read()
@@ -28,20 +35,16 @@ def tokenize_document(filename: str, lang: str) -> list[str]:
     for element in soup.find_all(["script", "style", ""]):
         element.decompose()
     
-    text = soup.get_text()
-    if lang in LANGUAGE_MAPPING:
-        language = LANGUAGE_MAPPING[lang]
-    else:
-        print(f"WARNING: No language mapping for {lang}; defaulting to English (en)...")
-        language = "english"
+    text = soup.get_text().lower()
     stop_words = stopwords.words(language)
     tokens = word_tokenize(text, language=language)
-    filtered_tokens = list(filter(lambda x: x.lower() not in stop_words and x not in SYMBOLS, tokens))
+    filtered_tokens = list(filter(lambda x: x not in stop_words and x not in SYMBOLS, tokens))
     return filtered_tokens
     
     
-def stem_tokens(tokens: list[str]) -> list[str]:
-    return[stemmer.stem(word) for word in tokens]
+def stem_tokens(tokens: list[str], language: str) -> list[str]:
+    stemmer = SnowballStemmer(language)
+    return [stemmer.stem(word) for word in tokens]
 
 
 def nltk_download():
@@ -72,8 +75,9 @@ if __name__ == "__main__":
     for url, outlinks, lang, filename in report_csv:
         # Tokenize and stem document to get words
         print(f"Tokenizing {filename} ({lang}): ", end="")
-        tokens = tokenize_document(filename, lang)
-        stems = stem_tokens(tokens)
+        language = map_language(lang)
+        tokens = tokenize_document(filename, language)
+        stems = stem_tokens(tokens, language)
 
         # Write words to file
         words_filename = filename.replace(".html", "-words.txt")
